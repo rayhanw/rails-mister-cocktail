83857e275a8bb8063cfed4e9e6cdc78becbadd01
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2019-05-24 11:26:06 +0800

    removed new_dose_path and dose#new

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,8 +1,8 @@
 class DosesController < ApplicationController
-  def new
-    @cocktail = Cocktail.find(params[:cocktail_id])
-    @dose = Dose.new
-  end
+  # def new
+  #   @cocktail = Cocktail.find(params[:cocktail_id])
+  #   @dose = Dose.new
+  # end
 
   def create
     @dose = Dose.new(dose_params)
