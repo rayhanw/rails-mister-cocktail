5a32775b9c9382712da71daa70f51ad38e4eb9f3
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2019-05-26 15:09:03 +0800

    customized alerts

--- app/javascript/components/alert.js
+++ app/javascript/components/alert.js
@@ -1,10 +1,11 @@
-const hideAfterTime = (milliseconds) => {
+const hideAfterTime = (seconds) => {
   const alert = document.querySelector('.alert');
+
 
   setTimeout(() => {
     alert.style.display = 'none';
-    alert.style.transition = 'linear .3s';
-  }, milliseconds * 1000);
+    alert.style.transition = `linear ${seconds}s`
+  }, seconds * 1000);
 };
 
 export { hideAfterTime };
